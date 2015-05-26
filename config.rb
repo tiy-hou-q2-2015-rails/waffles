887143d731e8dba4c23b4d9b452eb14489caa513
Jesse Wolgamott <jesse@comalproductions.com>
2015-05-26 07:45:57 -0500

    Add deploy

--- config.rb
+++ config.rb
@@ -65,8 +65,13 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true # default: false
 end
